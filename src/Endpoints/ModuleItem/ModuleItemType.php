bffb0f7782c6630c3a198b82e1952db7d05583f6
⚒ Anton Boutkam <anton@nui-boutkam.nl>
2025-01-09 12:49:21 +0100

    Added a class with assignment types (completed)

--- src/Endpoints/ModuleItem/ModuleItemType.php
+++ src/Endpoints/ModuleItem/ModuleItemType.php
@@ -9,6 +9,9 @@ class ModuleItemType extends CanvasObject
 
     public const ASSIGNMENT = 'Assignment';
     public const FILE = 'File';
+    public const PAGE = 'Page';
+    public const SUB_HEADER = 'SubHeader';
+    public const EXTERNAL_URL = 'ExternalUrl';
     public const DISCUSSION = 'Discussion';
     public const QUIZ = 'Quiz';
     public const EXTERNALTOOL = 'ExternalTool';
