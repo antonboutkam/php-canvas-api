4687a9d8abece745ed3f38a920fa07dadb3b4f24
Anton Boutkam <anton@nui-boutkam.nl>
2025-01-27 21:26:42 +0100

    Added README.md

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -6,5 +6,8 @@ require dirname(__DIR__, 1) . '/vendor/autoload.php';
 use Hurah\Types\Type\Path;
 use Symfony\Component\Console\Application;
 
-$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__, 1));
-$dotenv->load();
+if(file_exists(dirname(__DIR__, 1) . '.env')) {
+	// $_ENV variables may be defined in another way
+	$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__, 1));
+	$dotenv->load();
+}
